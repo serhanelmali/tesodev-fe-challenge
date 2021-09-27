aa7e8e447ce6b3286c2aa930d224883ae6c0435b
serhanelmali <serhanelmali@gmail.com>
2021-09-27 20:14:33 +0300

    fix(searchPerson): adds city to Person model

--- backend/utils/searchPerson.js
+++ backend/utils/searchPerson.js
@@ -5,27 +5,22 @@ const searchPerson = (persons, searchValue, results) => {
 
   for (let i = 0; i < persons.length; i++) {
     const person = persons[i];
+    let personToAdd;
 
     for (let j = 0; j < person.length; j++) {
-      if (
-        person[j].toLowerCase().includes(searchValue.toLowerCase()) &&
-        !results.includes(person)
-      ) {
-        results.push(
-          new Person(
-            person[0],
-            person[1],
-            person[2],
-            person[3],
-            person[4],
-            person[5]
-          )
+      if (person[j].toLowerCase().includes(searchValue.toLowerCase())) {
+        personToAdd = new Person(
+          person[0],
+          person[1],
+          person[2],
+          person[3],
+          person[4],
+          person[5]
         );
       }
     }
+    console.log(personToAdd);
   }
-
-  console.log(results);
 };
 
 module.exports = searchPerson;
