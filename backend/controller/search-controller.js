5dee9b472b351f1762cf9ea336173dea7b73b488
serhanelmali <serhanelmali@gmail.com>
2021-09-27 20:35:24 +0300

    refactor(search-controller): changes res.json format

--- backend/controller/search-controller.js
+++ backend/controller/search-controller.js
@@ -6,9 +6,15 @@ const sendData = async (req, res) => {
   const searchTerm = req.query.searchterm;
   try {
     searchPerson(personData.data, searchTerm, searchResults);
-    res.json(searchResults);
+    res.json({
+      message: `Search completed successfully. ${searchResults.length} results found.`,
+      data: searchResults,
+    });
   } catch (err) {
-    console.log(err);
+    res.json({
+      message: `An error occured.`,
+      error: err,
+    });
   }
 };
 
