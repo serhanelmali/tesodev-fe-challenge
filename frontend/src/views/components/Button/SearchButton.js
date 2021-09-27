3008889f8e3487fa1c45d25e1710b6172818e34c
serhanelmali <serhanelmali@gmail.com>
2021-09-27 09:45:10 +0300

    feat(Homepage, SearchButton): add Homepage, SearchButton Component

---
+++ frontend/src/views/components/Button/SearchButton.js
@@ -0,0 +1,7 @@
+import "./searchbutton.scss";
+
+const SearchButton = () => {
+  return <button>Search</button>;
+};
+
+export default SearchButton;
---
+++ frontend/src/views/pages/Homepage/index.js
@@ -0,0 +1,23 @@
+import logo from "../../../assets/tesodevlogo.jpg";
+import SearchButton from "../../components/Button/SearchButton";
+
+import "./homepage.scss";
+
+const Homepage = () => {
+  return (
+    <div className="container">
+      <div className="container__top">
+        <div className="container__top__wrapper">
+          <img className="container__top__logo" src={logo} alt="Tesodev Logo" />
+          <span className="container__top__text">Search web app</span>
+        </div>
+      </div>
+      <div className="container__middle">
+        <SearchButton />
+      </div>
+      <div className="container__bottom"></div>
+    </div>
+  );
+};
+
+export default Homepage;
