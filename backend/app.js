054c764066c83e2bc1900c0aee11af62f6a59359
serhanelmali <serhanelmali@gmail.com>
2021-09-28 10:37:43 +0300

    feat(context): creates context

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const PORT = process.env.PORT;
 const express = require("express");
+const cors = require("cors");
 const searchController = require("./controller/search-controller");
 
 const app = express();
+
+app.use(cors());
 
 app.use("/search", searchController.sendData);
 
