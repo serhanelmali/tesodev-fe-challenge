f80ad06c5532537fe0b226759e0a67b053e21118
serhanelmali <serhanelmali@gmail.com>
2021-09-28 10:50:00 +0300

    feat(fetchPersons): adds fetchPersons function to store

--- frontend/src/context/store.js
+++ frontend/src/context/store.js
@@ -1,14 +1,40 @@
 import { createContext, useContext, useState } from "react";
+import getPersons from "../api/getPersons";
 
 const AppContext = createContext();
 
 export function AppWrapper({ children }) {
   const [searchTerm, setSearch] = useState("az");
-  const [datas, setDatas] = useState({
+  const [persons, setPersons] = useState({
     data: [],
     isLoading: false,
     error: null,
   });
+
+  const fetchPersons = () => {
+    setPersons({
+      data: [],
+      isLoading: true,
+      error: null,
+    });
+
+    getPersons({
+      onSucces(result) {
+        setPersons({
+          data: result.data,
+          isLoading: false,
+          error: null,
+        });
+      },
+      onError(error) {
+        setPersons({
+          data: [],
+          error: error,
+          isLoading: false,
+        });
+      },
+    });
+  };
 
   let sharedState = {
     searchTerm,
