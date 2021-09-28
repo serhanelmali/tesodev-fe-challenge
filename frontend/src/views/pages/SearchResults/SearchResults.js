da0d15153d2f68ec941b9003b98e5dd9f3b5858f
serhanelmali <serhanelmali@gmail.com>
2021-09-28 22:43:12 +0300

    feat(SearchResults): adds search result to result page

--- frontend/src/views/pages/SearchResults/SearchResults.js
+++ frontend/src/views/pages/SearchResults/SearchResults.js
@@ -1,9 +1,14 @@
+import { useAppContext } from "../../../context/store";
+import PersonInfo from "../../components/PersonInfo/PersonInfo";
 import SearchBar from "../../layouts/SearchBar/SearchBar";
 import logo from "../../../assets/tesodevlogo.jpg";
 
 import "./searchresults.scss";
 
 const SearchResults = () => {
+  const store = useAppContext();
+  const persons = store?.persons.data;
+
   return (
     <div className="search-results__container">
       <div className="search-results__container__wrapper">
@@ -13,6 +18,21 @@ const SearchResults = () => {
         <div className="search-results__container__wrapper__right">
           <div className="search-results__container__wrapper__right__search">
             <SearchBar />
+          </div>
+          <div className="search-result__container__wrapper__right__person-list">
+            {persons &&
+              persons
+                .slice(0, 6)
+                .map((person, id) => (
+                  <PersonInfo
+                    country={person.country}
+                    city={person.city}
+                    email={person.email}
+                    fullname={person.fullname}
+                    date={person.date}
+                    key={id}
+                  />
+                ))}
           </div>
         </div>
       </div>
